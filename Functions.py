5b4e10103cd879acaa47f7e91e2dac608d144754
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-09-30 17:25:39 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,44 @@
+# Input/Output Functions
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+# Type Conversion Functions
+num_str = input("Enter a number: ")
+num = int(num_str)
+print("Double of your number:", num * 2)
+
+# Mathematical Functions
+print("Absolute value of -5:", abs(-5))
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+print("Max:", max(numbers))
+print("Min:", min(numbers))
+print("Sum:", sum(numbers))
+
+# Sequence Functions
+my_list = [1, 2, 3, 4, 5]
+print("Length of list:", len(my_list))
+
+# Sequence Manipulation Functions
+words = ["apple", "banana", "cherry"]
+sorted_words = sorted(words)
+print("Sorted words:", sorted_words)
+
+# String Functions
+sentence = "   This is a sentence with whitespace.   "
+print("Uppercase:", sentence.upper())
+print("Lowercase:", sentence.lower())
+print("Stripped:", sentence.strip())
+
+# List Functions
+fruits = ["apple", "banana", "cherry"]
+fruits.append("date")
+print("Appended list:", fruits)
+
+# Dictionary Functions
+person = {'name': 'Priya', 'age': 30}
+print("Keys:", person.keys())
+print("Values:", person.values())
+
+
+
+
