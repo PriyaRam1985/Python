ab19b82a342439d925bc2abee25c8a9797c19269
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-09-30 16:55:09 +0530

    Add files via upload

---
+++ DataTypesOfPython,py.py
@@ -0,0 +1,33 @@
+# Integer
+my_integer = 42
+
+# Floating-point number
+my_float = 3.14
+
+# String
+my_string = "Hello, World!"
+
+# List (mutable, ordered collection)
+my_list = [1, 2, 3, 4, 5]
+
+# Tuple (immutable, ordered collection)
+my_tuple = (10, 20, 30, 40, 50)
+
+# Dictionary (key-value pairs)
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+
+# Boolean
+my_bool = True
+
+# NoneType
+my_none = None
+
+# Printing the data types and their values
+print(f"Integer: {my_integer}, type: {type(my_integer)}")
+print(f"Float: {my_float}, type: {type(my_float)}")
+print(f"String: {my_string}, type: {type(my_string)}")
+print(f"List: {my_list}, type: {type(my_list)}")
+print(f"Tuple: {my_tuple}, type: {type(my_tuple)}")
+print(f"Dictionary: {my_dict}, type: {type(my_dict)}")
+print(f"Boolean: {my_bool}, type: {type(my_bool)}")
+print(f"NoneType: {my_none}, type: {type(my_none)}")
