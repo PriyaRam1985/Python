28a39fa4ca25670d50a21735e6885e3f4f71c278
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-09-30 19:08:06 +0530

    Add files via upload

---
+++ PythonStatement1.py
@@ -0,0 +1,34 @@
+# Declare and initialize variables
+x = 10
+y = 5
+
+# Perform arithmetic operations
+sum = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+
+# Print the results
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+
+# Use conditional statements
+if x > y:
+    print("x is greater than y")
+else:
+    print("y is greater than or equal to x")
+
+# Use a loop
+for i in range(5):
+    print("Iteration:", i)
+
+# Define a function
+def greet(name):
+    print("Hello,", name)
+
+# Call the function
+greet("Anudip")
+
+
