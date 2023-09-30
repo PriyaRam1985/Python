98e622f1d36fdf667ae6b92389c1518ecff12696
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-09-30 15:44:14 +0530

    Add files via upload

---
+++ PythonOperators.py
@@ -0,0 +1,37 @@
+list1  = [1, "hi", "Anudio", 2]    
+#Checking type of given list  
+print(type(list1))  
+  
+#Printing the list1  
+print (list1)  
+  
+# List slicing  
+print (list1[3:])  
+  
+# List slicing  
+print (list1[0:2])   
+  
+# List Concatenation using + operator  
+print (list1 + list1)  
+  
+# List repetation using * operator  
+print (list1 * 3)  
+
+tup  = ("hi", "Anudip", 2)    
+# Checking type of tup  
+print (type(tup))    
+  
+#Printing the tuple  
+print (tup)  
+  
+# Tuple slicing  
+print (tup[1:])    
+print (tup[0:1])    
+  
+# Tuple concatenation using + operator  
+print (tup + tup)    
+  
+# Tuple repatation using * operator  
+print (tup * 3)     
+  
+
