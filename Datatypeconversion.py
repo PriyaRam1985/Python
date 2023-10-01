0edfffe093cd7af42dac2f8c6e5c8bf0773607ea
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-10-01 13:37:47 +0530

    Add files via upload

---
+++ Datatypeconversion.py
@@ -0,0 +1,17 @@
+# Convert string to integer
+num_str = "42"
+num_int = int(num_str)
+print(num_str)
+
+# Convert integer to string
+num_str2 = str(num_int)
+print(num_int)
+
+# Convert float to integer
+num_float = 3.14
+num_int2 = int(num_float)
+print(num_float)
+
+# Convert integer to float
+num_float2 = float(num_int2)
+print(num_int2)
