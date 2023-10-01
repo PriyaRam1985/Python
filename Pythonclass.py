b3ad4a524e3a5e1dc816508944c40bb50ab337a9
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-10-01 14:47:49 +0530

    Add files via upload

---
+++ Pythonclass.py
@@ -0,0 +1,16 @@
+
+
+class Anudip:
+    def __init__(self, name, course):
+        self.name = name
+        self.course = course
+       
+
+# Creating two Dog objects
+Anudip1 = Anudip("Java", "APJFSA")
+Anudip2 = Anudip("Tally", "GST")
+
+# Accessing object attributes
+print(Anudip1.name)  
+print(Anudip2.course)  
+
