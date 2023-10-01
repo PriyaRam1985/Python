330faeb5af4350fbc1a783d2cae39e554568fe48
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-10-01 15:00:38 +0530

    Add files via upload

---
+++ IteratorandDate.py
@@ -0,0 +1,15 @@
+
+#Use Of Iterator
+
+mytuple = ("Java", "Python", "JavaVsPython")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# Use Of DateTime
+
+import datetime
+x = datetime.datetime.now()
+print(x)
