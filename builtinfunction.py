f306197146c9e73d0c14762961918a325b2ea137
PriyaRam1985 <144799016+PriyaRam1985@users.noreply.github.com>
2023-10-01 13:32:20 +0530

    Add files via upload

---
+++ builtinfunction.py
@@ -0,0 +1,21 @@
+# Generate a list of numbers from 0 to 9
+numbers = list(range(10))
+print(numbers)
+
+# Generate Absolute Value
+num = -5
+abs_value = abs(num)
+print("Absolute value:", abs_value)
+
+# Sum() to calculate the sum of elements in a lsit
+
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print("Sum:", total)
+
+# min() and max()->minimum and maximum value in a list
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+max_value = max(numbers)
+min_value = min(numbers)
+print("Max:", max_value)
+print("Min:", min_value)
